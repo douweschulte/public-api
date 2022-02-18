6502538317380714ea0336b7a028efed2be4b237
Martin Nordholts <enselic@gmail.com>
2022-02-18 07:16:03 +0100

    wip

--- src/implementation.rs
+++ src/implementation.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use rustdoc_types::Crate;
 
 use crate::PuuuublicIttttem;
@@ -12,19 +14,21 @@ pub use item_iterator::PublicItem;
 pub fn public_items_from_rustdoc_json_str(
     crate_: &Crate,
 ) -> Result<impl Iterator<Item = crate::PuuuublicIttttem> + '_> {
-
-    Ok(ItemIterator::new(&crate_)
-        .map(|i| i.as_ref())
-        .map(intermediate_public_item_to_public_item))
+    Ok(ItemIterator::new(&crate_).map(intermediate_public_item_to_public_item))
 }
 
-fn intermediate_public_item_to_public_item(public_item: &PublicItem) -> PuuuublicIttttem {
+fn intermediate_public_item_to_public_item<'a>(
+    public_item: Rc<PublicItem<'a>>,
+) -> PuuuublicIttttem {
     let prefix = public_item.prefix();
+
     let path = public_item
         .path()
         .iter()
         .map(|i| i.get_effective_name())
-        .collect::<Vec<String>>();
+        .collect::<Vec<String>>()
+        .join("::");
+
     let suffix = public_item.suffix();
 
     PuuuublicIttttem {
