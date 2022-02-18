f39530054244bafce8d6f9da9e671b41990452f5
Martin Nordholts <enselic@gmail.com>
2022-02-18 20:24:48 +0100

    wip

--- tests/rustdoc_json/bless.sh
+++ tests/rustdoc_json/bless.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o nounset -o pipefail -o errexit
 
+base="tests/rustdoc_json"
+
 for input in syntect-v4.6.0 thiserror-v1.0.30; do
-    printf "%s" "$(cargo run tests/rustdoc_json/${input}_FORMAT_VERSION_10.json)" >! "tests/rustdoc_json/${input}-expected.txt"
+    echo "$(cargo run ${base}/${input}_FORMAT_VERSION_10.json)" >! "${base}/${input}-expected.txt"
 done
